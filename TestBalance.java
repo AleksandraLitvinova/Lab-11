c53cf9d51f4153198b4e62a8f7398dc83d6ab382
AleksandraLitvinova <44903736+AleksandraLitvinova@users.noreply.github.com>
2019-03-01 19:00:11 +0700

    Add files via upload

---
+++ TestBalance.java
@@ -0,0 +1,11 @@
+package MyPack1;
+
+import MyPack.*;
+class TestBalance {
+    public static void main(String args[]) {
+     /*  ласс Balance объ€влен как public, поэтому им можно
+     воспользоватьс€ и вызвать его конструктор . */
+        Balance test = new Balance("J.J. Jaspe rs",99.88);
+        test.show(); // можно также вызвать метод show()
+    }
+}
